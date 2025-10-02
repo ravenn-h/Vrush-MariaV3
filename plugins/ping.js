eda3a450ca00060df6ca968718079b50e8d77d73
hhhisoka <enochkouame123@gmail.com>
2025-10-02 23:27:09 +0000

    update ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,14 @@
+const handler = async (m, { sock }) => {
+  const start = new Date().getTime()
+  await m.reply("Pinging...")
+  const end = new Date().getTime()
+  const responseTime = end - start
+  m.reply(`🏓 Pong!\n> Response
+  time: ${responseTime}ms`)
+}
+
+handler.help = ["ping"]
+handler.tags = ["info"]
+handler.command = ["ping"]
+
+export default handler
